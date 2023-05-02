afdeebd621f625a16baf919b14513cefa4991a1e
Lucas <luckofahl@gmail.com>
2023-05-01 21:25:18 -0300

    bug update corrigido

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,7 +3,7 @@ const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  /**não precisa colocar o "function", pois a "class" já saber que é uma "functions" */
+  /**não precisa colocar o "function", pois a "class" já sabe que é uma "functions" */
   async create(request, response) {
     const { name, email, password } = request.body;
 
@@ -29,25 +29,25 @@ class UsersController {
     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
 
     if (!user) {
-      throw new AppError("Usuário não encontrado");
+      throw new AppError("Usuário não encontrado.");
     }
 
     const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
     if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
-      throw new AppError("E-mail já em uso.");
+      throw new AppError("E-mail já em uso");
     }
 
     user.name = name;
     user.email = email;
 
     await database.run(`
-    UPDATE users SET
-    name = ?,
-    email = ?,
-    update_at = ?,
-    WHERE id = ?`,
-    [user.name, user.email, new Date(), id]
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
     );
 
     return response.status(200).json();
