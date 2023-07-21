e003afb15d1577f8fdcd6ecb94fa997061948e02
Lucas <luckofahl@gmail.com>
2023-07-20 22:57:53 -0300

    atualização

--- src/database/knex/migrations/20230508235329_createNotes.js
+++ src/database/knex/migrations/20230508235329_createNotes.js
@@ -1,7 +1,7 @@
 
 exports.up = knex => knex.schema.createTable("notes", table => {
   table.increments("id");
-  table.text("Title");
+  table.text("title");
   table.text("description");
   table.integer("user_id").references("id").inTable("users");
 
